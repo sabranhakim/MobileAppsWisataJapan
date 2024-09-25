807c1fea3db573a4be5dd45b354068b78c815b63
Sabran Hakim <sabranhakim00@gmail.com>
2024-09-25 09:20:25 +0700

    update splash screen

---
+++ app/src/main/java/com/sabran/mobileappswisatajapan/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.sabran.mobileappswisatajapan
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnSplashScreen : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        btnSplashScreen = findViewById(R.id.btnSplashScreen)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        btnSplashScreen.setOnClickListener() {
+            val intentMenu5 = Intent(this@MainActivity, HomePage::class.java)
+            startActivity(intentMenu5)
+        }
+    }
+}
