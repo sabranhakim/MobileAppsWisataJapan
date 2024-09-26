f5e8dded3e3144090c2afcd6d412dfc54e9a69e1
Sabran Hakim <sabranhakim00@gmail.com>
2024-09-27 01:32:41 +0700

    update detail

--- app/src/main/java/com/sabran/mobileappswisatajapan/MainActivity.kt
+++ app/src/main/java/com/sabran/mobileappswisatajapan/MainActivity.kt
@@ -7,27 +7,31 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.sabran.mobileappswisatajapan.adapter.WisataAdapter
+import com.sabran.mobileappswisatajapan.model.WisataList
+import com.sabran.mobileappswisatajapan.model.WisataModel
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnSplashScreen : Button
+    private lateinit var rv_wisata : RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        btnSplashScreen = findViewById(R.id.btnSplashScreen)
+        rv_wisata= findViewById(R.id.rv_wisata)
+
+        rv_wisata.layoutManager = GridLayoutManager(this,  2)
+        val adapter = WisataAdapter(WisataList.getModel() as ArrayList<WisataModel> , this)
+        rv_wisata.adapter = adapter
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        btnSplashScreen.setOnClickListener() {
-            val intentMenu5 = Intent(this@MainActivity, HomePage::class.java)
-            startActivity(intentMenu5)
         }
     }
 }
